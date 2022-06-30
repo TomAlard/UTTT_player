27b93a4def6c1935bdb4fabfbbac99224f1e6240
Tom Alard <tom.alard@ugent.be>
2022-06-30 14:17:41 +0200

    test(bitboard): reCurse vs DaFish simulation test

--- test/bitboard_tests.c
+++ test/bitboard_tests.c
@@ -24,11 +24,48 @@ void nineOrEightMovesAllowedAfterFirstMove(BitBoard* bitBoard) {
 }
 
 
+Square to_our_notation(Square rowAndColumn) {
+    uint8_t row = rowAndColumn.board;
+    uint8_t column = rowAndColumn.position;
+    uint8_t board = 3 * (row / 3) + (column / 3);
+    uint8_t position = 3 * (row % 3) + (column % 3);
+    Square result = {board, position};
+    return result;
+}
+
+
+// https://www.codingame.com/replay/296363090
+void reCurseVsDaFish() {
+    BitBoard* bitBoard = createBitBoard();
+    Square playedMoves[49] = {
+            {4, 4}, {5, 3}, {6, 2}, {1, 7}, {5, 4}, {8, 3}, {8, 0}, {8, 1}, {7, 3}, {4, 2}, {3, 7}, {1, 5}, {5, 7},
+            {7, 5}, {4, 7}, {3, 4}, {0, 5}, {1, 6}, {3, 2}, {1, 8}, {3, 3}, {2, 0}, {7, 1}, {5, 5}, {7, 7}, {4, 5},
+            {1, 4}, {3, 5}, {2, 3}, {4, 0}, {4, 1}, {1, 0}, {3, 0}, {2, 2}, {7, 6}, {5, 2}, {6, 6}, {0, 0}, {8, 5},
+            {8, 6}, {7, 4}, {8, 7}, {8, 4}, {6, 3}, {7, 8}, {6, 4}, {6, 5}, {5, 0}, {3, 1}
+    };
+    int possibleMoves[49] = {
+            81, 8, 9, 9, 7, 9, 8, 7, 8, 9, 9, 9, 8,
+            7, 7, 6, 8, 8, 8, 7, 49, 9, 6, 4, 9, 3,
+            38, 2, 35, 29, 6, 27, 5, 7, 8, 4, 7, 6, 15,
+            6, 13, 12, 4, 3, 9, 5, 4, 3, 2
+    };
+    Square ignoredGeneratedMoves[TOTAL_SMALL_SQUARES];
+    for (int i = 0; i < 49; i++) {
+        myAssert(generateMoves(bitBoard, ignoredGeneratedMoves) == possibleMoves[i]);
+        Square move = to_our_notation(playedMoves[i]);
+        myAssert(makeMove(bitBoard, move) == (i != 48? NONE : WIN_P1));
+    }
+    freeBitBoard(bitBoard);
+}
+
+
 void runBitBoardTests() {
     BitBoard* bitBoard = createBitBoard();
     printf("\tanyMoveAllowedOnEmptyBoard...\n");
     anyMoveAllowedOnEmptyBoard(bitBoard);
     printf("\tnineOrEightMovesAllowedAfterFirstMove...\n");
     nineOrEightMovesAllowedAfterFirstMove(bitBoard);
+    printf("\treCurseVsDaFish...\n");
+    reCurseVsDaFish();
     freeBitBoard(bitBoard);
 }
