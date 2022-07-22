94633c9e6aaf79108e575c5b907ad9aca11ec911
Tom Alard <tom.alard@ugent.be>
2022-07-22 08:58:35 +0200

    refactor(random): make public function private

--- src/random.h
+++ src/random.h
@@ -8,8 +8,6 @@ typedef struct pcg32_random_t {
     uint64_t inc;
 } pcg32_random_t;
 
-uint32_t pcg32_random_r(pcg32_random_t* rng);
-
 void pcg32_srandom_r(pcg32_random_t* rng, uint64_t init_state, uint64_t init_seq);
 
 uint32_t pcg32_boundedrand_r(pcg32_random_t* rng, uint32_t bound);
