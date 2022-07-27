6db030b475d9ff6e1f05ade822df587754546d3f
Tom Alard <tom.alard@ugent.be>
2022-07-27 11:36:09 +0200

    test(profile_simulations): increase amount of runs, make runs independent

--- test/profile_simulations.c
+++ test/profile_simulations.c
@@ -4,18 +4,19 @@
 
 
 void profileSimulations() {
-    Board* board = createBoard();
-    MCTSNode* root = createMCTSRootNode();
-    RNG rng;
-    seedRNG(&rng, 69, 420);
-    Square square = createSquare(1, 0);
-    root = updateRoot(root, board, square);
-    makePermanentMove(board, square);
+    const int runs = 100;
     int totalSims = 0;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < runs; i++) {
+        Board* board = createBoard();
+        MCTSNode* root = createMCTSRootNode();
+        RNG rng;
+        seedRNG(&rng, 69, 420);
+        Square square = createSquare(1, 0);
+        root = updateRoot(root, board, square);
+        makePermanentMove(board, square);
         totalSims += findNextMove(board, root, &rng, 0.1);
+        freeMCTSTree(root);
+        freeBoard(board);
     }
-    printf("%d\n", totalSims / 10);
-    freeMCTSTree(root);
-    freeBoard(board);
+    printf("%d\n", totalSims / runs);
 }
