68f4c80fed3a4b63d12d729d230a4537de0ff6e3
Tom Alard <tom.alard@ugent.be>
2022-07-27 11:48:49 +0200

    optimization(player_bitboard): precalculate wins

--- src/player_bitboard.c
+++ src/player_bitboard.c
@@ -15,7 +15,26 @@ typedef struct PlayerBitBoard {
 } PlayerBitBoard;
 
 
+bool precalculatedIsWin[512];
+bool isWin_(uint16_t smallBoard) {
+    __m128i masks = _mm_setr_epi16(0x7, 0x38, 0x1c0, 0x49, 0x92, 0x124, 0x111, 0x54);
+    __m128i v1 = _mm_set1_epi16(1);
+    __m128i boards128 = _mm_set1_epi16((short) smallBoard);
+    __m128i andResult = _mm_and_si128(masks, boards128);
+    __m128i result = _mm_cmpeq_epi16(andResult, masks);
+    return !_mm_test_all_zeros(result, v1);
+}
+
+
+bool isWin(uint16_t smallBoard) {
+    return precalculatedIsWin[smallBoard];
+}
+
+
 PlayerBitBoard* createPlayerBitBoard() {
+    for (uint16_t i = 0; i < 512; i++) {
+        precalculatedIsWin[i] = isWin_(i);
+    }
     return safe_calloc(sizeof(PlayerBitBoard));
 }
 
@@ -42,16 +61,6 @@ uint16_t getSmallBoard(PlayerBitBoard* playerBitBoard, uint8_t board) {
 
 bool squareIsOccupied(PlayerBitBoard* playerBitBoard, Square square) {
     return BIT_CHECK(playerBitBoard->smallBoards[square.board], square.position);
-}
-
-
-bool isWin(uint16_t smallBoard) {
-    __m128i masks = _mm_setr_epi16(0x7, 0x38, 0x1c0, 0x49, 0x92, 0x124, 0x111, 0x54);
-    __m128i v1 = _mm_set1_epi16(1);
-    __m128i boards128 = _mm_set1_epi16((short) smallBoard);
-    __m128i andResult = _mm_and_si128(masks, boards128);
-    __m128i result = _mm_cmpeq_epi16(andResult, masks);
-    return !_mm_test_all_zeros(result, v1);
 }
 
 
