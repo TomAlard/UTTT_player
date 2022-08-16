7d4d524ae12b60080de6015dc0e08ce0f3589bf7
Tom Alard <tom.alard@ugent.be>
2022-08-16 11:08:34 +0200

    feat(codingame_file): update (rank 17)

--- codingame_file.c
+++ codingame_file.c
@@ -320,8 +320,7 @@ void shuffle(int* array, int n, RNG* rng) {
 
 
 // START PLAYER_BIT_BOARD
-bool precalculatedIsWin[512];
-bool isWin_(uint16_t smallBoard) {
+bool isWin(uint16_t smallBoard) {
     __m128i masks = _mm_setr_epi16(0x7, 0x38, 0x1c0, 0x49, 0x92, 0x124, 0x111, 0x54);
     __m128i v1 = _mm_set1_epi16(1);
     __m128i boards128 = _mm_set1_epi16((short) smallBoard);
@@ -331,15 +330,7 @@ bool isWin_(uint16_t smallBoard) {
 }
 
 
-bool isWin(uint16_t smallBoard) {
-    return precalculatedIsWin[smallBoard];
-}
-
-
 void initializePlayerBitBoard(PlayerBitBoard* playerBitBoard) {
-    for (uint16_t i = 0; i < 512; i++) {
-        precalculatedIsWin[i] = isWin_(i);
-    }
     memset(playerBitBoard, 0, sizeof(PlayerBitBoard));
 }
 
@@ -1221,6 +1212,38 @@ int findNextMove(Board* board, MCTSNode* root, RNG* rng, double allocatedTime) {
 
 
 // START HANDLE_TURN
+Square handleOpening(Board* board) {
+    uint8_t ply = getPly(board);
+    Square invalid = {9, 9};
+    if (board->me != PLAYER2 || ply > 10) {
+        return invalid;
+    }
+    bool firstMove = BIT_CHECK(board->state.player1.smallBoards[4], 4);
+    bool secondMove = BIT_CHECK(board->state.player1.smallBoards[8], 8);
+    bool thirdMove = BIT_CHECK(board->state.player1.smallBoards[0], 0);
+    bool fourthMove = BIT_CHECK(board->state.player1.smallBoards[5], 5);
+    bool fifthMove = BIT_CHECK(board->state.player1.smallBoards[7], 7);
+    uint8_t currentBoard = getCurrentBoard(board);
+    if (ply == 1 && firstMove) {
+        Square move = {4, 8};
+        return move;
+    } else if (ply == 3 && firstMove && secondMove && currentBoard == 8) {
+        Square move = {8, 0};
+        return move;
+    } else if (ply == 5 && firstMove && secondMove && thirdMove && currentBoard == 0) {
+        Square move = {0, 5};
+        return move;
+    } else if (ply == 7 && firstMove && secondMove && thirdMove && fourthMove && currentBoard == 5) {
+        Square move = {5, 7};
+        return move;
+    } else if (ply == 9 && firstMove && secondMove && thirdMove && fourthMove && fifthMove && currentBoard == 7) {
+        Square move = {7, 5};
+        return move;
+    }
+    return invalid;
+}
+
+
 MCTSNode* handleEnemyTurn(Board* board, MCTSNode* root, Square enemyMove, RNG* rng) {
     if (enemyMove.board == 9 && enemyMove.position == 9) {
         setMe(board, PLAYER1);
@@ -1235,6 +1258,13 @@ MCTSNode* handleEnemyTurn(Board* board, MCTSNode* root, Square enemyMove, RNG* r
 
 HandleTurnResult handleTurn(Board* board, MCTSNode* root, RNG* rng, double allocatedTime, Square enemyMove) {
     root = handleEnemyTurn(board, root, enemyMove, rng);
+    Square openingMove = handleOpening(board);
+    if (openingMove.board != 9) {
+        MCTSNode* newRoot = updateRoot(root, board, openingMove);
+        makePermanentMove(board, openingMove);
+        HandleTurnResult result = {openingMove, newRoot, 0};
+        return result;
+    }
     int amountOfSimulations = findNextMove(board, root, rng, allocatedTime);
     Square move = getMostPromisingMove(root);
     MCTSNode* newRoot = updateRoot(root, board, move);
