417d19c31e8570f3478ff804f2a7f15397b9fb60
Tom Alard <tom.alard@ugent.be>
2022-10-14 10:32:49 +0200

    feat(neural_network): balance positions data

--- src/arena/neural_network/merge_games.py
+++ src/arena/neural_network/merge_games.py
@@ -1,7 +1,8 @@
 import os
+import csv
 
 
-def main():
+def merge():
     all_positions = []
     for _, _, files in os.walk("../games"):
         for filename in files:
@@ -19,5 +20,27 @@ def main():
             f.write(position + '\n')
 
 
+def balance():
+    amounts = [0, 0, 0]
+    with open('../positions.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)  # Skip header
+        for row in reader:
+            winner_index = int(row[-1]) - 1
+            amounts[winner_index] += 1
+    target = min(amounts)
+    current = [0, 0, 0]
+    with open('../positions.csv', 'r') as read_file:
+        reader = csv.reader(read_file, delimiter=',')
+        with open('../balanced_positions.csv', 'w') as write_file:
+            write_file.write(','.join(next(reader)) + '\n')  # Write header
+            for row in reader:
+                winner_index = int(row[-1]) - 1
+                if current[winner_index] < target:
+                    current[winner_index] += 1
+                    write_file.write(','.join(row) + '\n')
+
+
 if __name__ == '__main__':
-    main()
+    merge()
+    balance()
